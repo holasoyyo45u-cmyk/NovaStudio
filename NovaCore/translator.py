42fa7b9699493c773081749416857bbb88eb8150
holasoyyo45u-cmyk <holasoyyo45u@gmail.com>
2026-07-25 16:28:11 -0600

    Update translator.py

--- NovaCore/translator.py
+++ NovaCore/translator.py
@@ -1,1 +1,29 @@
+import json
 
+# Cargar comandos
+with open("NovaCore/commands.json", "r") as file:
+    data = json.load(file)
+
+commands = data["commands"]
+
+def translate(text):
+    text = text.lower()
+
+    for name, info in commands.items():
+        if name.replace("_", " ") in text:
+            return {
+                "command": name,
+                "action": info["action"]
+            }
+
+    return {
+        "command": "unknown",
+        "action": "None"
+    }
+
+
+# Prueba
+while True:
+    user = input("Nova > ")
+    result = translate(user)
+    print(result)
