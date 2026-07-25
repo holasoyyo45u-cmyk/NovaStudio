6a432f59460027bbce3cb635f8cfda4ff3294d35
holasoyyo45u-cmyk <holasoyyo45u@gmail.com>
2026-07-25 16:29:28 -0600

    Update server.py

--- API/server.py
+++ API/server.py
@@ -1,1 +1,23 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
+class NovaServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        response = {
+            "status": "online",
+            "message": "Nova Core funcionando"
+        }
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(json.dumps(response).encode())
+
+
+server = HTTPServer(("0.0.0.0", 8080), NovaServer)
+
+print("Nova API iniciada")
+
+server.serve_forever()
