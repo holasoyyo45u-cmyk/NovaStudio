31c105ca62db8cc7e838909243f2ddb397f2c316
holasoyyo45u-cmyk <holasoyyo45u@gmail.com>
2026-07-25 16:30:11 -0600

    Update examples.lua

--- Luau/examples.lua
+++ Luau/examples.lua
@@ -1,1 +1,21 @@
+-- Nova Studio Luau Examples
 
+local Examples = {}
+
+Examples.CreatePart = function()
+	local part = Instance.new("Part")
+	part.Size = Vector3.new(4,4,4)
+	part.Position = Vector3.new(0,5,0)
+	part.Anchored = true
+	part.Parent = workspace
+	
+	return part
+end
+
+
+Examples.ChangeColor = function(part, color)
+	part.Color = color
+end
+
+
+return Examples
